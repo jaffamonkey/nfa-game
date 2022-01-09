1fe23c9be586228de09e9758661918eb5c86b603
pablo <pablo@pablos-mbp.home>
2022-01-09 00:52:52 +0000

    added new graphics

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -2,7 +2,7 @@ import styles from './logo.css';
 import React from 'react';
 
 function Logo() {
-    return <h1 className={styles.root}>The monster's vault</h1>
+    return <h1 className={styles.root}>No Forwarding Address</h1>
 }
 
 export default Logo;
